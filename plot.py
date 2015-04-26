95c140d71c8c6ea93fa3513cd3a14ec07f0b64aa
Eric Leong <eric@ericleong.me>
2015-04-26 17:40:13 -0400

    Handle headerless case, use double quotes.

--- plot.py
+++ plot.py
@@ -7,30 +7,35 @@ import matplotlib.pyplot as plt
 
 totals = []
 details = []
-headers = []
+headers = ["Draw", "Prepare", "Process", "Execute"]
 
 for line in fileinput.input():
-    if fileinput.isfirstline():
-        headers = line.strip().split('\t')
-    else:
-        values = map(float, line.split('\t'))
 
-        for i, val in enumerate(values):
-            try:
-                details[i].append(val)
-            except:
-                details.append([val])
+    try:
+        values = map(float, line.split("\t"))
+    except:
+        if fileinput.isfirstline():
+            headers = line.strip().split("\t")
+            continue
+
+    totals.append(sum(values))
+
+    for i, val in enumerate(values):
+        try:
+            details[i].append(val)
+        except:
+            details.append([val])
 
 if len(totals) > 0 and len(details) > 0:
 
     # Draw, Prepare, Process, Execute
-    colors = ['cornflowerblue', 'purple', 'orangered', 'orange']
+    colors = ["cornflowerblue", "purple", "orangered", "orange"]
     time = range(len(totals))
     threshold = 16.67;
 
     # histogram of frame delays
     plt.subplot2grid((3, 2), (0, 0))
-    plt.hist(totals, range(int(math.floor(min(totals))), int(math.ceil(max(totals))) + 1), color='limegreen')
+    plt.hist(totals, range(int(math.floor(min(totals))), int(math.ceil(max(totals))) + 1), color="limegreen")
     plt.title("Distribution of Frame Rendering Times")
     plt.xlabel("Total Render Time (ms)")
     plt.ylabel("Frequency")
@@ -47,10 +52,10 @@ if len(totals) > 0 and len(details) > 0:
     ax = plt.subplot2grid((3, 2), (1, 0), colspan=2)
     for i, (detail, column, color) in enumerate(zip(details, headers, colors)):
         ax.bar(time, detail, label=column, color=color, linewidth=0, bottom=[0] * len(detail) if i == 0 else map(sum, zip(*details[:i])), width=1.0)
-    ax.plot([0, len(frames)], [threshold, threshold], color='limegreen')
+    ax.plot([0, len(totals)], [threshold, threshold], color="limegreen")
     plt.title("Render Time Series")
     plt.xlabel("Frame Number")
-    plt.xlim([0, len(frames)])
+    plt.xlim([0, len(totals)])
     plt.ylabel("Render Time (ms)")
     ax.legend()
 
@@ -60,10 +65,10 @@ if len(totals) > 0 and len(details) > 0:
     ax = plt.subplot2grid((3, 2), (2, 0), colspan=2)
     for i, (detail, column, color) in enumerate(zip(duration_curves, headers, colors)):
         ax.bar(time, detail, label=column, color=color, linewidth=0, bottom=[0] * len(detail) if i == 0 else map(sum, zip(*duration_curves[:i])), width=1.0)
-    ax.plot([0, len(frames)], [threshold, threshold], color='limegreen')
+    ax.plot([0, len(totals)], [threshold, threshold], color="limegreen")
     plt.title("Render Duration Curve")
     plt.xlabel("Frame Number")
-    plt.xlim([0, len(frames)])
+    plt.xlim([0, len(totals)])
     plt.ylabel("Render Time (ms)")
     ax.legend()
 
