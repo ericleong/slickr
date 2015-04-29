61ca22b11e9ddcd01938660dc431beef8990d4ff
Eric Leong <eric@ericleong.me>
2015-04-28 23:56:56 -0400

    Clearer arguments.

--- slickr.sh
+++ slickr.sh
@@ -3,18 +3,27 @@
 # Specify the package name of the Android app you want to track
 # as the first argument.
 
-# Swipe by 3x the density of the device.
-# TODO: account for landscape
-VERTICAL=$(expr $(adb shell wm density | grep -o '[0-9]\+') \* 3)
+# Number of times to run
+if [ "$2" = "" ] ; then
+	COUNT=4
+else
+	COUNT=$2
+fi
 
-# Run 4 times
-for i in {1..${2:-4}}
+# Vertical pixels to swipe
+if [ "$3" = "" ] ; then
+	VERTICAL=$(expr $(adb shell wm density | grep -o '[0-9]\+') \* 3)
+else
+	VERTICAL=$3
+fi
+
+for i in {1..$COUNT}
 do
     # Swipe 8 times for 250 ms each.
     # This works out to 2000 ms, which is about the duration of 128 frames at 60 FPS
     for i in {1..8}
     do
-        adb shell input touchscreen swipe 100 ${3:-$VERTICAL} 100 0 250
+        adb shell input touchscreen swipe 100 $VERTICAL 100 0 250
     done
     adb shell dumpsys gfxinfo $1 | ./profile.py
 done
