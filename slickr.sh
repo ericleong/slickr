cc8d7df9b68758acb5d02b7d1d8c450e3fc7f52e
Eric Leong <eric@ericleong.me>
2016-06-27 19:32:25 -0400

    Add support for ICS devices.

--- slickr.sh
+++ slickr.sh
@@ -31,13 +31,32 @@ fi
 
 # Android Marshmallow features
 # http://developer.android.com/preview/testing/performance.html#timing-info
-VERSION=$(adb shell getprop ro.build.version.release | cut -c 1)
+VERSION=$(adb shell getprop ro.build.version.release)
+MAJOR_VERSION=$(echo $VERSION | cut -c 1)
+MINOR_VERSION=$(echo $VERSION | cut -c 3)
 if [ "$1" != "" ] ; then
     # Test if integer, then test if >= 6.0
-    if [[ "$VERSION" =~ ^-?[0-9]+$ ]] && [ "$VERSION" -ge "6" ] ; then
+    if [[ "$MAJOR_VERSION" =~ ^-?[0-9]+$ ]] && [ "$MAJOR_VERSION" -ge "6" ] ; then
         FRAMESTATS="framestats"
-    elif [ "$VERSION" == "N" ] ; then # N preview
+    elif [ "$MAJOR_VERSION" == "N" ] ; then # N preview
         FRAMESTATS="framestats"
+    fi
+fi
+
+# Scroll command differs by version
+
+SWIPE="touchscreen swipe"
+DURATION="250"
+
+if [[ "$MAJOR_VERSION" =~ ^-?[0-9]+$ ]] ; then
+    if [ "$MAJOR_VERSION" -ge "5" ] ; then
+        :
+    elif [ "$MAJOR_VERSION" -ge "4" ] && [ "$MINOR_VERSION" -gt "1" ] ; then
+        :
+    else
+    	# Old device
+        SWIPE="swipe"
+        DURATION=""
     fi
 fi
 
@@ -51,7 +70,7 @@ if [ $COUNT -gt "1" ] ; then
     # adb shell is a little slow, so when this is finished,
     # about 128 frames (2 seconds at 60 fps) should have passed.
     # Afterwards, dump data and filter for profile data
-    adb shell "for i in `seq -s ' ' 1 $COUNT`; do for j in `seq -s ' ' 1 3`; do input touchscreen swipe 100 $VERTICAL 100 0 250; done; dumpsys gfxinfo $1 $FRAMESTATS; done;" | ./profile.py
+    adb shell "for i in `seq -s ' ' 1 $COUNT`; do for j in `seq -s ' ' 1 3`; do input $SWIPE 100 $VERTICAL 100 0 $DURATION; done; dumpsys gfxinfo $1 $FRAMESTATS; done;" | ./profile.py
 else
-    adb shell "for j in `seq -s ' ' 1 3`; do input touchscreen swipe 100 $VERTICAL 100 0 250; done; dumpsys gfxinfo $1 $FRAMESTATS;" | ./profile.py
+    adb shell "for j in `seq -s ' ' 1 3`; do input $SWIPE 100 $VERTICAL 100 0 $DURATION; done; dumpsys gfxinfo $1 $FRAMESTATS;" | ./profile.py
 fi
